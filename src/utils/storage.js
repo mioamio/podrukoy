9153263de17c30a39efe4ca338d6c38d650a2f93
mioamio <98706916+mioamio@users.noreply.github.com>
2025-01-18 22:31:06 +0300

    Delete src directory

--- src/utils/storage.js
+++
@@ -1,7 +0,0 @@
-export const saveData = (key, data) => {
-  localStorage.setItem(key, JSON.stringify(data));
-};
-
-export const getData = (key) => {
-  return JSON.parse(localStorage.getItem(key));
-};
