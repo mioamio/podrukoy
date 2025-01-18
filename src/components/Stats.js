892167c72878dd7eea656452838433d934b914c2
mioamio <98706916+mioamio@users.noreply.github.com>
2025-01-18 22:21:15 +0300

    Create Stats.js

---
+++ src/components/Stats.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Stats = () => {
+  return (
+    <div className="stats">
+      <h2>Статистика</h2>
+      <p>Графики и данные будут здесь.</p>
+    </div>
+  );
+};
+
+export default Stats;
